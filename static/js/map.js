fc9639b9d0c67388921237e89e537f651cb1d101
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2022-12-21 17:22:28 +0100

    Sviluppo template

---
+++ static/js/map.js
@@ -0,0 +1,56 @@
+window.onload = function(){
+    const MAP_KEY = takeMapKey();
+    const URL = takeUrl();
+    var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = URL + '/js?v=3&key='+ MAP_KEY +'&callback=documentReady';
+	document.body.appendChild(script);
+
+    setTimeout(setMap, 1000);
+    
+}
+
+function setMap(){
+    var div = document.getElementsByClassName("wrapper")[0];
+    var posizione = new google.maps.LatLng(44.5558363,7.733851);
+    var mapOptions = {
+    center:posizione,
+    zoom:16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP // default=ROADMAP
+    };
+    var mappa = new google.maps.Map(div, mapOptions); 
+
+    var icon1 = {
+        url: '/static/img/marker.png',
+        scaledSize: new google.maps.Size(30, 40), // dimensioni
+        // anchor: new google.maps.Point(30, 40), // posizione
+        // origin: new google.maps.Point(0,0), // file con icone multiple
+    }
+        
+
+    let markerOptions = {
+        map:mappa,
+        position:posizione,
+        title:"IIS Vallauri", // tooltip
+        label:"", // lettera maiuscola singola
+        icon:icon1, // complex icon
+        zIndex: 3, // in caso di marcatori vicini/sovrapposti
+        animation: google.maps.Animation.DROP, // .BOUNCE oscilla
+        draggable:false, // rende il marcatore trascinabile
+        }
+
+        let marcatore1 = new google.maps.Marker(markerOptions)
+        
+        let markerOptions2 = {
+            map:mappa,
+            position:new google.maps.LatLng(44.6558363,7.733851),
+            title:"IIS Vallauri", // tooltip
+            label:"", // lettera maiuscola singola
+            icon:icon1, // complex icon
+            zIndex: 3, // in caso di marcatori vicini/sovrapposti
+            animation: google.maps.Animation.DROP, // .BOUNCE oscilla
+            draggable:false, // rende il marcatore trascinabile
+            }
+
+            new google.maps.Marker(markerOptions2)
+}
---
+++ static/js/template.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    let pages = $('.sec');
+
+    pages.eq(0).on("click", () => {window.location.href = "index.html";})
+    pages.eq(1).on("click", () => {window.location.href = "perizie.html";})
+    pages.eq(2).on("click", () => {window.location.href = "map.html";})
+}
