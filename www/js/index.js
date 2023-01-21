38608282869a57bea69960bfe1af4745d1c27b39
PietroBossolasco <p.bossolasco.1715@vallauri.edu>
2023-01-21 20:17:56 +0100

    Sviluppo pagina home

--- www/js/index.js
+++ www/js/index.js
@@ -22,8 +22,5 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    
 }
